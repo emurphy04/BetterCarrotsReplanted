16bea9a253dd8f5bce8994b1b8598a8666cb0504
Ethan Murphy <ethanmurphy267@gmail.com>
2025-02-12 17:24:16 -0330

    Configured Classes

---
+++ src/main/java/com/bettercarrots/BettercarrotsreplantedClient.java
@@ -0,0 +1,10 @@
+package com.bettercarrots;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BettercarrotsreplantedClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
