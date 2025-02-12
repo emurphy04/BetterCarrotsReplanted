d991823fe46f79bb04864fec3e41e2dcd57dd42f
Ethan Murphy <ethanmurphy267@gmail.com>
2025-02-12 16:59:43 -0330

    Created initial project

---
+++ src/main/java/com/bettercarrots/BettercarrotsreplantedDataGenerator.java
@@ -0,0 +1,11 @@
+package com.bettercarrots;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BettercarrotsreplantedDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
