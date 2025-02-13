38186ab3103a6ae78020ea2f0be4409829264eb0
Ethan Murphy <ethanmurphy267@gmail.com>
2025-02-12 21:07:01 -0330

    Configured Classes

--- src/main/java/com/bettercarrots/item/ModItems.java
+++ src/main/java/com/bettercarrots/item/ModItems.java
@@ -15,10 +15,26 @@ import net.minecraft.util.Identifier;
 public class ModItems {
 
     public static final Item TEST = registerItem("test", new Item(new FabricItemSettings()));
+    //diamond
+    public static final Item DIAMOND_CARROT = registerItem("diamond_carrot", new Item(new FabricItemSettings()));
 
-    private static void addItemsToIngredientsTabItemGroup(FabricItemGroupEntries entries){
-        entries.add(TEST);
-    }
+    //iron
+    public static final Item IRON_CARROT = registerItem("iron_carrot", new Item(new FabricItemSettings()));
+
+    //lapis
+    public static final Item LAPIS_CARROT = registerItem("lapis_carrot", new Item(new FabricItemSettings()));
+
+    //redstone
+    public static final Item REDSTONE_CARROT = registerItem("redstone_carrot", new Item(new FabricItemSettings()));
+
+    //netherite
+    public static final Item NETHERITE_CARROT = registerItem("netherite_carrot", new Item(new FabricItemSettings()));
+
+    //rilynium
+    public static final Item RILYNIUM_CARROT = registerItem("rilynium_carrot", new Item(new FabricItemSettings()));
+
+    //emerald
+    public static final Item EMERALD_CARROT = registerItem("emerald_carrot", new Item(new FabricItemSettings()));
 
     private static Item registerItem(String name, Item item){
         return Registry.register(Registries.ITEM, new Identifier(Bettercarrotsreplanted.MOD_ID, name), item);
@@ -26,6 +42,5 @@ public class ModItems {
 
     public static void registerModItems(){
         Bettercarrotsreplanted.LOGGER.info("Registering Mod Items");
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addItemsToIngredientsTabItemGroup);
     }
 }
